72339ef921c1d199a4d627613f0a1ed741377408
linzhou <38331396+linzhoucool@users.noreply.github.com>
2018-09-17 12:24:01 -0500

    Update Armor.cs

--- OOP_RPG/Armor.cs
+++ OOP_RPG/Armor.cs
@@ -3,9 +3,11 @@ namespace OOP_RPG
 {
     public class Armor
     {
-        public Armor(string name, int defense, int originalValue, int resellValue)
+        public Armor(string name,int number, int defense, int originalValue, int resellValue)
         {
+            
             this.Name = name;
+            this.Number = number;
             this.Defense = defense;
             this.OriginalValue = originalValue;
             this.ResellValue = resellValue;
@@ -13,8 +15,10 @@ namespace OOP_RPG
         }
         
         public string Name { get; set; }
+        public int Number { get; set; }
         public int Defense { get; set; }
         public int OriginalValue { get; set; }
         public int ResellValue { get; set; }
+      
     }
 }
