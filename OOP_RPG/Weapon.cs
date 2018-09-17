f1b64888d19a59dfbf8afe6a30ff476c1c98b579
linzhou <38331396+linzhoucool@users.noreply.github.com>
2018-09-17 12:27:44 -0500

    Update Weapon.cs

--- OOP_RPG/Weapon.cs
+++ OOP_RPG/Weapon.cs
@@ -3,10 +3,10 @@ namespace OOP_RPG
 {
     public class Weapon
     {
-        public Weapon(string name, int strength , int originalValue , int resellValue ) {
+        public Weapon(int number,string name,  int strength , int originalValue , int resellValue ) {
             this.Name = name;
             this.Strength = strength;
-
+            this.Number = number;
             this.OriginalValue = originalValue;
             this.ResellValue = resellValue;
         }
@@ -15,7 +15,7 @@ namespace OOP_RPG
         public int Strength { get; set; }
         public int OriginalValue { get; set; }
         public int ResellValue { get; set; }
-
+        public int Number { get; set; }
 
 
 
