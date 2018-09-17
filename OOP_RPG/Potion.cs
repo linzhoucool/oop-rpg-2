6d10d8c8e38fb67a9b8bf60832531295219a9420
linzhou <38331396+linzhoucool@users.noreply.github.com>
2018-09-17 12:27:05 -0500

    Update Potion.cs

--- OOP_RPG/Potion.cs
+++ OOP_RPG/Potion.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -10,11 +10,11 @@ namespace OOP_RPG
         public string Name { get; set; }
         public int OriginalValue { get; set; }
         public int ResellValue { get; set; }
+        public int Number { get; set; }
 
-
-        public Potion(string name, int hp , int originalValue, int resellValue)
+        public Potion(string name, int number,int hp, int originalValue, int resellValue)
         {
-
+            this.Number = number;
             this.Name = name;
             this.HP = hp;
             this.OriginalValue = originalValue;
